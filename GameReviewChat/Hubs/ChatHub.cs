1774256e26230a7bc2f16e5e881bd1f7d00ffe39
Zandzakje <DamianTomasila@hotmail.com>
2021-01-06 10:27:26 +0100

    Start tutorial

---
+++ GameReviewChat/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameReviewChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage(string username, string message)
+        {
+            return Clients.All.SendAsync("ReceiveOne", username, message);
+        }
+    }
+}
